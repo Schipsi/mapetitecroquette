87833eb51026d4aa3e34f1da9bb1de881bc0383e
Schipsi <thibaut.juston@thetribe.io>
2022-04-01 21:01:10 +0200

    Add initial symfony install

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(): Response
+    {
+        return $this->render('base.html.twig');
+    }
+}
