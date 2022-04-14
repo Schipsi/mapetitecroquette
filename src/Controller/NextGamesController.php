5c781f289879a312f19dcb1ea57b044d41c801a3
Schipsi <thibaut.juston@thetribe.io>
2022-04-14 21:49:22 +0200

    Add current game and improve performance

--- src/Controller/NextGamesController.php
+++ src/Controller/NextGamesController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Game;
+use App\Entity\User;
 use App\Repository\GameRepository;
+use App\Repository\PredictionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +13,36 @@ use Symfony\Component\Routing\Annotation\Route;
 class NextGamesController extends AbstractController
 {
     #[Route('/next-games', name: 'next-games')]
-    public function show(GameRepository $gameRepository): Response
+    public function show(GameRepository $gameRepository, PredictionRepository $predictionRepository): Response
     {
-        $games = $gameRepository->findBy(['state' => [Game::STATE_UNSTARTED, Game::STATE_STARTED]], ['date' => 'ASC']);
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $currentGame = $gameRepository->findOneBy(['state' => Game::STATE_STARTED], ['date' => 'ASC']);
+        $games = $gameRepository->findBy(['state' => Game::STATE_UNSTARTED], ['date' => 'ASC']);
+
+        $predictions = [];
+        foreach ($games as $key => $game) {
+            $prediction = $predictionRepository->findOneBy(['user' => $user, 'game' => $game]);
+
+            if (null !== $prediction) {
+                $predictions[$key] = $prediction->getTeam();
+            } else {
+                $predictions[$key] = null;
+            }
+        }
+
+        $currentGamePrediction = $predictionRepository->findOneBy(['user' => $user, 'game' => $currentGame]);
+
+        if (null !== $currentGamePrediction) {
+            $currentGamePrediction = $currentGamePrediction->getTeam();
+        }
 
         return $this->render('page/next_games.html.twig', [
             'games' => $games,
+            'currentGame' => $currentGame,
+            'predictions' => $predictions,
+            'currentGamePrediction' => $currentGamePrediction,
         ]);
     }
 }
