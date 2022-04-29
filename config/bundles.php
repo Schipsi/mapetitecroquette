58e570d90e33ff9d05e09461ef946d583894a10d
Schipsi <thibaut.juston@thetribe.io>
2022-04-29 18:12:33 +0200

    Add sentry

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
